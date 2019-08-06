a64b810a71ac284c54eee1a5ab8b42898176b287
Alfred Tuffreau <tuffreau.alfred@gmail.com>
2019-08-06 16:26:34 +0200

    Fix title html tag in Catcher component

--- src/components/Catcher.js
+++ src/components/Catcher.js
@@ -5,7 +5,7 @@ import { formattedText } from "../translations";
 
 export default () => (
   <div className="Catcher">
-    <h2>{ formattedText("app.catcher.title") }</h2>
+    <h1>{ formattedText("app.catcher.title") }</h1>
     <p>{ formattedText("app.catcher.description") }</p>
   </div>
 );
