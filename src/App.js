f4ec73c0b68285702f24a6735a35d8837b2afb5e
Alfred Tuffreau <tuffreau.alfred@gmail.com>
2020-03-28 13:09:23 +0100

    removed background handler in nav relicate

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import { Home, Login, Recipes, Recipe, AboutUs, ContactUs, LostPassword, NotFoun
 
 export default () => (
   <Routes routes={[
-    { path: HOME, component: Home, require: UNAUTH, withBackground: true },
-    { path: HOME2, component: Home, require: UNAUTH, withBackground: true },
-    { path: LOGIN, component: Login, require: UNAUTH, withBackground: true },
+    { path: HOME, component: Home, require: UNAUTH },
+    { path: HOME2, component: Home, require: UNAUTH },
+    { path: LOGIN, component: Login, require: UNAUTH },
     { path: RECIPES, component: Recipes, require: AUTH },
     { path: RECIPE, component: Recipe, require: AUTH },
-    { path: ABOUT_US, component: AboutUs, withBackground: true },
-    { path: CONTACT_US, component: ContactUs, withBackground: true },
-    { path: LOST_PASSWORD, component: LostPassword, withBackground: true },
-    { component: NotFound, withBackground: true },
+    { path: ABOUT_US, component: AboutUs },
+    { path: CONTACT_US, component: ContactUs },
+    { path: LOST_PASSWORD, component: LostPassword },
+    { component: NotFound },
   ]} />
 );
